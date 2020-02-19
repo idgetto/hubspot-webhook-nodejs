bcb2c10f2727a2cfacef90c97de54021196458ec
Isaac Getto <igetto@hubspot.com>
2020-02-19 14:45:50 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const crypto = require('crypto');
+
+http.createServer((request, response) => {
+  let body = [];
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+
+    const appSecret = "dfc56f62-8629-464e-a27a-78ac540c9a2f";
+    const httpMethod = "POST";
+    const URI = "localhost:8080";
+
+    const concatedValue = appSecret + httpMethod + URI + body
+
+    const hash = crypto.createHash('sha256').update(concatedValue).digest('hex');
+
+    console.log(body);
+
+    response.end(hash);
+  });
+
+}).listen(8080);
